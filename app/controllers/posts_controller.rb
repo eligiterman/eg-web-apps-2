bc5bcf3d9e6703124f6d9da9c8fa5e1176a40e55
Eli Giterman <eli.giterman@kellogg.northwestern.edu>
2025-05-13 16:31:41 +0000

    Class from 5/13

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   def index
     # find all Post rows
-    @posts = Post.all
+    @post = Post.all
     # render posts/index view
   end
 
@@ -10,11 +10,20 @@ class PostsController < ApplicationController
     # render posts/new view with new Post form
   end
 
-  # def create
-  #   # start with a new Post
-  #   # assign user-entered form data to Post's columns
-  #   # save Post row
-  #   # redirect user
-  # end
+  def create
+    # start with a new Post
+    @post = Post.new
+
+    # assign user-entered form data to Post's columns
+    @post["author"] = params["author"]
+    @post["image"] = params["image"]
+    @post["body"] = params["body"]
+
+    # save Post row
+    @post.save
+
+    # redirect user
+    redirect_to "/posts"
+  end
 
 end
